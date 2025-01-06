b39c855849a7b60a837fa630a4edb2fe2b721fed
mizza-cz <mizza.cz666@gmail.com>
2025-01-06 09:52:48 +0100

    add fixes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -15,3 +15,20 @@ function headerNavOpenerClick() {
     }
   });
 }
+
+
+ let scrollpos = window.scrollY
+  const header = document.querySelector(".header")
+  const header_height = header.offsetHeight
+
+  const add_class_on_scroll = () => header.classList.add("fade-in")
+  const remove_class_on_scroll = () => header.classList.remove("fade-in")
+
+  window.addEventListener('scroll', function() { 
+    scrollpos = window.scrollY;
+
+    if (scrollpos >= header_height) { add_class_on_scroll() }
+    else { remove_class_on_scroll() }
+
+    console.log(scrollpos)
+  })
