ef25e4abb6f71ee5147ce7a014ad6ea921596ff4
mizza-cz <mizza.cz666@gmail.com>
2025-01-11 13:28:25 +0100

    fix select

--- src/js/custom/select2.js
+++ src/js/custom/select2.js
@@ -2,10 +2,3 @@ $(".js-select").select2({
   width: "100%",
   minimumResultsForSearch: -1,
 });
-
-$(function () {
-  //presmerovani pomoci selectu
-  $(".js-select").change(function () {
-    location.href = $(this).val();
-  });
-});
