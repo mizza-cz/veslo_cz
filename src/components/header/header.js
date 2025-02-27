786a328c202a2c6d7d3c4dd9ed117d184fde8406
mizza-cz <mizza.cz666@gmail.com>
2025-02-27 07:50:32 +0100

    add fixes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -16,19 +16,19 @@ function headerNavOpenerClick() {
   });
 }
 
+let scrollpos = window.scrollY;
+const header = document.querySelector(".header");
+const header_height = header.offsetHeight;
 
- let scrollpos = window.scrollY
-  const header = document.querySelector(".header")
-  const header_height = header.offsetHeight
+const add_class_on_scroll = () => header.classList.add("fade-in");
+const remove_class_on_scroll = () => header.classList.remove("fade-in");
 
-  const add_class_on_scroll = () => header.classList.add("fade-in")
-  const remove_class_on_scroll = () => header.classList.remove("fade-in")
+window.addEventListener("scroll", function () {
+  scrollpos = window.scrollY;
 
-  window.addEventListener('scroll', function() { 
-    scrollpos = window.scrollY;
-
-    if (scrollpos >= header_height) { add_class_on_scroll() }
-    else { remove_class_on_scroll() }
-
-    console.log(scrollpos)
-  })
+  if (scrollpos >= header_height) {
+    add_class_on_scroll();
+  } else {
+    remove_class_on_scroll();
+  }
+});
--- src/js/custom/select2.js
+++ src/js/custom/select2.js
@@ -2,3 +2,5 @@ $(".js-select").select2({
   width: "100%",
   minimumResultsForSearch: -1,
 });
+
+ 
